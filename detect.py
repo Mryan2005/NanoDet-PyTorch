6ed413536470694cc752b388c3ab364c773e74fc
Zhizhong Yan <A2564011261@163.com>
2024-08-14 07:31:20 +0000

    add(detect.py): add it to recalled it

---
+++ detect.py
@@ -0,0 +1,65 @@
+import cv2
+import os
+import time
+import torch
+import argparse
+from nanodet.util import cfg, load_config, Logger
+from nanodet.model.arch import build_model
+from nanodet.util import load_model_weight
+from nanodet.data.transform import Pipeline
+
+class Predictor(object):
+    def __init__(self, cfg, model_path, logger, device='cuda:0'):
+        self.cfg = cfg
+        self.device = device
+        model = build_model(cfg.model)
+        ckpt = torch.load(model_path, map_location=lambda storage, loc: storage)
+        load_model_weight(model, ckpt, logger)
+        self.model = model.to(device).eval()
+        self.pipeline = Pipeline(cfg.data.val.pipeline, cfg.data.val.keep_ratio)
+
+    def inference(self, img):
+        img_info = {}
+        if isinstance(img, str):
+            img_info['file_name'] = os.path.basename(img)
+            img = cv2.imread(img)
+        else:
+            img_info['file_name'] = None
+
+        height, width = img.shape[:2]
+        img_info['height'] = height
+        img_info['width'] = width
+        meta = dict(img_info=img_info,
+                    raw_img=img,
+                    img=img)
+        meta = self.pipeline(meta, self.cfg.data.val.input_size)
+        meta['img'] = torch.from_numpy(meta['img'].transpose(2, 0, 1)).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            results = self.model.inference(meta)
+        return meta, results
+
+    def visualize(self, dets, meta, class_names, score_thres, wait=0):
+        time1 = time.time()
+        self.model.head.show_result(meta['raw_img'], dets, class_names, score_thres=score_thres, show=True)
+        print('viz time: {:.3f}s'.format(time.time()-time1))
+
+
+def get_image_list(path):
+    image_names = []
+    for maindir, subdir, file_name_list in os.walk(path):
+        for filename in file_name_list:
+            apath = os.path.join(maindir, filename)
+            ext = os.path.splitext(apath)[1]
+            if ext in image_ext:
+                image_names.append(apath)
+    return image_names
+
+def detect():
+    load_config(cfg, "./config/nanodet-m.yml")
+    logger = Logger(-1, use_tensorboard=False)
+    predictor = Predictor(cfg, "model/nanodet_m.pth", logger, device='cpu')
+    return predictor
+
+def predict(predictor, img):
+    meta, res = predictor.inference(img)
+    return meta, res
